057ee01b0eba6e4b26bfd51edaa142e651a3fc83
Musa Rafik <musa.amer.rafik@gmail.com>
2022-04-26 22:17:00 -0500

    implemented getAllGroceryItems

--- controllers/grocery_controller.js
+++ controllers/grocery_controller.js
@@ -1,11 +1,28 @@
 let GroceryItem = require("../models/grocery_item");
 
+const errorMessage = (err) => {
+    return {
+        "error": err,
+        "status code": 400
+    };
+}
+
 const getGroceryItemById = (req, res) => {
     console.log(req.params.id);
 }
 
 const getAllGroceryItems = (req, res) => {
-    console.log("need to implement");
+    GroceryItem.find((err, groceryItems) => {
+        if(err){
+            res.send(errorMessage(err));
+        }
+        else{
+            res.send({
+                "grocery items": groceryItems,
+                "status code": 200
+            });
+        }
+    });
 }
 
 const deleteGroceryItemById = (req, res) => {
@@ -20,10 +37,7 @@ const addGroceryItem = (req, res) => {
     let newGroceryItem = new GroceryItem(req.body);
     newGroceryItem.save((err) => {
         if(err){
-            res.send({
-                "error": err,
-                "status code": 400
-            });
+            res.send(errorMessage(err));
         }
         else{
             console.log(`Saved ${newGroceryItem.quantity} of ${newGroceryItem.name} which is reoccurring ${newGroceryItem.isReoccurring}`);
